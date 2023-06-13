e227d709cc65fbd650c424d641731b1e8372c82c
Gabriel <wiedermann.gabriel@gmail.com>
2023-06-13 15:55:16 +0200

    Added Class Tour, CreateTourLogViewModel, IoCContainerConfig, RelayCommand

---
+++ UI/ViewModels/SideMenuViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace UI.ViewModels
+{
+    public class SideMenuViewModel
+    {
+        public ICommand AddTour { get; }
+        public ICommand ModifyTour { get; }
+        public ICommand DeleteTour { get; }
+    }
+}
