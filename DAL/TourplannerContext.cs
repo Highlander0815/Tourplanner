1c40e10efbfc45ed0b304146fce639458aa3044e
Oliver Müller <if21b017@technikum-wien.at>
2023-07-12 16:37:40 +0200

    Updated class diagrams and documentation

--- DAL/TourplannerContext.cs
+++ DAL/TourplannerContext.cs
@@ -10,11 +10,5 @@ namespace DAL
 
         public DbSet<TourModel> Tours { get; set; }
         public DbSet<TourLogModel> Tourlogs { get; set; }
-
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseNpgsql(ConfigurationManager.ConnectionStrings["tour_db"].ConnectionString);
-        //    AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-        //}
     }
 }
